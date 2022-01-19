7e4e7b578b86e225969d6c655851aa2277c55e55
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-01-20 00:06:29 +0530

    Update texture maps

--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -9,9 +9,10 @@ import { useSpring, animated } from '@react-spring/three';
 // Styles
 import '@pages/demo/Demo.scss';
 import Header from '@components/Header/Header';
-import earthmap from '@assets/images/mars_uv_map.jpeg';
-import ringmap from '@assets/images/rings.jpeg';
+import earthmap from '@assets/images/globe.png';
+import ringmap from '@assets/images/rings.png';
 import spacemap from '@assets/images/space_bg.png';
+import cloudsmap from '@assets/images/globe_clouds.png';
 
 // soft Shadows
 softShadows();
@@ -19,17 +20,17 @@ softShadows();
 const Demo = () => {
     const earthTexture = React.useMemo(() => new THREE.TextureLoader().load(earthmap), [earthmap]);
     const ringsTexture = React.useMemo(() => new THREE.TextureLoader().load(ringmap), [ringmap]);
-    const spaceTexture = React.useMemo(() => new THREE.TextureLoader().load(spacemap), [spacemap]);
+    const cloudsTexture = React.useMemo(() => new THREE.TextureLoader().load(cloudsmap), [cloudsmap]);
 
     const meshRef = React.useRef();
 
     return (
-        <div id="canvas-container" style={{ backgroundImage: `url(${spacemap})` }}>
+        <div id="canvas-container">
             <Header />
             {/* Our Scene & Camera is already built into our canvas */}
-            <Canvas shadows camera={{ position: [-1, 2, 10], fov: 60 }}>
+            <Canvas shadows camera={{ position: [-1, 2, 10], fov: 55 }}>
                 {/* This light makes things look pretty */}
-                <ambientLight intensity={0.3} />
+                <ambientLight intensity={0.8} />
                 {/* Our main source of light, also casting our shadow */}
                 <pointLight position={[0, 100, 0]} intensity={0.3} />
 
@@ -39,14 +40,27 @@ const Demo = () => {
                         <planeBufferGeometry attach="geometry" args={[100, 100]} />
                         <shadowMaterial attach="material" />
                     </mesh>
-                    <group position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
+                    <group position={[0, -5, 0]} rotation={[Math.PI / 2, 0, 0]}>
                         <mesh ref={meshRef}>
                             <sphereGeometry attach="geometry" args={[2, 100, 100]} />
                             <meshLambertMaterial attach="material" map={earthTexture} />
                         </mesh>
                         <mesh ref={meshRef}>
-                            <ringGeometry attach="geometry" args={[3, 6, 40]} />
-                            <meshLambertMaterial attach="material" map={ringsTexture} side={THREE.DoubleSide} />
+                            <sphereGeometry attach="geometry" args={[2.06, 100, 100]} />
+                            <meshLambertMaterial
+                                attach="material"
+                                map={cloudsTexture}
+                                blending={THREE.AdditiveBlending}
+                            />
+                        </mesh>
+                        <mesh ref={meshRef}>
+                            <ringGeometry attach="geometry" args={[2.5, 8, 40]} />
+                            <meshLambertMaterial
+                                attach="material"
+                                map={ringsTexture}
+                                side={THREE.DoubleSide}
+                                blending={THREE.AdditiveBlending}
+                            />
                         </mesh>
                     </group>
                 </group>
