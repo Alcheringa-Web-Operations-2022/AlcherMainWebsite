0de0450a2bfd5cf4af5df873f4c8f4ee242b30f7
Shankar <gowrishankarirwog@gmail.com>
2022-01-23 19:22:07 +0530

    installed gsap

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import '@app/App.scss';
 import Banner from '@pages/demo/Banner';
+import From_33sec from '../routes/From_33sec';
 
 function App() {
     return (
-        <div>
-            <Banner />
-            <div
-                style={{
-                    width: '100vw',
-                    height: '100vh',
-                }}
-            ></div>
-        </div>
+        <Router>
+            <div>
+                {/* <div
+                    style={{
+                        width: '100vw',
+                        height: '100vh',
+                    }}
+                ></div> */}
+            </div>
+            <Routes>
+                <Route path="/" element={<Banner />} />
+                <Route path="/from_33sec" element={<From_33sec />} />
+            </Routes>
+        </Router>
     );
 }
 
---
+++ src/routes/From_33sec.js
@@ -0,0 +1,14 @@
+// const bg_green = require('../assets/from_33sec/bg-green.png');
+import bg_green from '../assets/backgrounds/bg-green.png';
+import '../assets/styles/from_33sec.css';
+
+const From_33sec = () => {
+    return (
+        <div>
+            <div></div>
+            {/* <div style={{ backgroundImage: `url(${bg_green})`, height: '100vh' }}>Hello</div> */}
+        </div>
+    );
+};
+
+export default From_33sec;
