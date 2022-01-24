4e5a40419fe4593f7b7448193fd058c51f7b99a7
VolDemorT-acer <kunalsolanke1000@gmail.com>
2022-01-24 13:41:18 +0530

    2nd animation part 1 complete

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -47,7 +47,7 @@ function LandingPage() {
             });
 
             tl.to('.section-text', { autoAlpha: 0, duration: 0.2 });
-            tl.to('.white__rings', { scale: 4, duration: 1 }, '>');
+            tl.to('.white__rings', { scale: 5, duration: 1 }, '>');
             tl.to('.video-container', { css: { clipPath: 'circle(105% at 50% 50%)' } }, '<');
         }
     }, [loading]);
